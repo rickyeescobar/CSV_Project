c950019f6ddd589a9a9c43e46a10595357631d58
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-02-22 13:25:08 -0600

    class

--- sitka_2.py
+++ sitka_2.py
@@ -1,4 +1,6 @@
 import csv
+from datetime import datetime
+
 
 open_file = open("sitka_weather_07-2018_simple.csv", "r")
 
@@ -13,21 +15,39 @@ for index, column_header in enumerate(header_row):
     print("Index:", index, "Column Name:",column_header)
 
 highs = []
+dates = []
+
+#as an example
+#mydate ='2018-07-01'
+#converted_date = datetime.strptime(mydate,'%Y-%m-%d')
+#print(converted_date)
 
 for row in csv_file:
     highs.append(int(row[5]))
+    converted_date = datetime.strptime(row[2],'%Y-%m-%d')
+    dates.append(converted_date)
+
 
 #print(highs)
+#print(dates)
+
 
 #plot highs on chart
 
 import matplotlib.pyplot as plt
 
-plt.plot(highs, c="red")
+
+fig=plt.figure()
+
+
+plt.plot(dates, highs, c="red")
+
+fig.autofmt_xdate()
+
 plt.title("Daily high temperatures, July 2018", fontsize=16)
-plt.xlabel("", fontsize=16)
-plt.ylabel("Temperature (F)", fontsize=16)
-plt.tick_params(axis="both", which="major", labelsize=16)
+plt.xlabel("", fontsize=12)
+plt.ylabel("Temperature (F)", fontsize=12)
+plt.tick_params(axis="both",labelsize=12)
 
 
 plt.show()
