d526fe420d6aceb8e1660b32bac8189e6bebc990
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-02-22 13:37:50 -0600

    sitka 3 changes

--- sitka_3.py
+++ sitka_3.py
@@ -1,8 +1,11 @@
+#1) change the file to include all the data for the year of 2018
+#2) fill in later
+
 import csv
 from datetime import datetime
 
 
-open_file = open("sitka_weather_07-2018_simple.csv", "r")
+open_file = open("sitka_weather_2018_simple.csv", "r")
 
 csv_file = csv.reader(open_file, delimiter= ',')
 
@@ -16,6 +19,7 @@ for index, column_header in enumerate(header_row):
 
 highs = []
 dates = []
+lows = []
 
 #as an example
 #mydate ='2018-07-01'
@@ -29,8 +33,10 @@ dates = []
 
 for row in csv_file:
     highs.append(int(row[5]))
+    lows.append(int(row[6]))
     converted_date = datetime.strptime(row[2],'%Y-%m-%d')
     dates.append(converted_date)
+    
 
 
 #print(highs)
@@ -44,12 +50,13 @@ import matplotlib.pyplot as plt
 
 fig=plt.figure()
 
+plt.plot(dates, highs, c="red",)
+plt.plot(dates, lows, c="blue")
 
-plt.plot(dates, highs, c="red")
 
 fig.autofmt_xdate()
 
-plt.title("Daily high temperatures, July 2018", fontsize=16)
+plt.title("Daily high and low temperatures - 2018", fontsize=16)
 plt.xlabel("", fontsize=12)
 plt.ylabel("Temperature (F)", fontsize=12)
 plt.tick_params(axis="both",labelsize=12)
