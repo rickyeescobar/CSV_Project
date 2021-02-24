914e90c32dae152bf98b3e7d9f10377f2d98154b
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-02-24 13:14:26 -0600

    added

--- diy_temps.py
+++ diy_temps.py
@@ -2,6 +2,22 @@ import csv
 from datetime import datetime
 
 
+open_file = open("sitka_weather_2018_simple.csv", "r")
+
+csv_file = csv.reader(open_file, delimiter= ',')
+
+header_row = next(csv_file)
+
+
+
+
+
+
+
+
+
+
+
 
 
 fig2, a = plt.subplots(2)
